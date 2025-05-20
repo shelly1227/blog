6ca7e9ace402ce8db780c6e5521e4d7f6c4d9ea6
shelly1227 <40505282@qq.com>
2025-05-20 16:43:12 +0800

    refactor: 去除不必要的 Elasticsearch 和 RabbitMQ 依赖，当前业务量无需引入

--- src/main/java/com/shelly/consumer/EmailConsumer.java
+++ src/main/java/com/shelly/consumer/EmailConsumer.java
@@ -1,47 +1,47 @@
-package com.shelly.consumer;
-
-import com.shelly.entity.dto.MailDTO;
-import com.shelly.service.impl.EmailService;
-import org.springframework.amqp.core.ExchangeTypes;
-import org.springframework.amqp.rabbit.annotation.Exchange;
-import org.springframework.amqp.rabbit.annotation.Queue;
-import org.springframework.amqp.rabbit.annotation.QueueBinding;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.stereotype.Component;
-
-import static com.shelly.constants.MqConstant.*;
-
-
-/**
- * 邮件消费者
- *
- * @author shelly
- */
-@Component
-public class EmailConsumer {
-
-    @Autowired
-    private EmailService emailService;
-
-    @RabbitListener(bindings = {
-            @QueueBinding(
-                    value = @Queue(value = EMAIL_SIMPLE_QUEUE, durable = "true", autoDelete = "false"),
-                    exchange = @Exchange(value = EMAIL_EXCHANGE, type = ExchangeTypes.TOPIC),
-                    key = EMAIL_SIMPLE_KEY
-            )})
-    public void listenSendSimpleEmail(@Payload MailDTO mail) {
-        emailService.sendSimpleMail(mail);
-    }
-
-    @RabbitListener(bindings = {
-            @QueueBinding(
-                    value = @Queue(value = EMAIL_HTML_QUEUE, durable = "true", autoDelete = "false"),
-                    exchange = @Exchange(value = EMAIL_EXCHANGE, type = ExchangeTypes.TOPIC),
-                    key = EMAIL_HTML_KEY
-            )})
-    public void listenSendHtmlEmail(@Payload MailDTO mail) {
-        emailService.sendHtmlMail(mail);
-    }
-}
+//package com.shelly.consumer;
+//
+//import com.shelly.entity.dto.MailDTO;
+//import com.shelly.service.impl.EmailService;
+//import org.springframework.amqp.core.ExchangeTypes;
+//import org.springframework.amqp.rabbit.annotation.Exchange;
+//import org.springframework.amqp.rabbit.annotation.Queue;
+//import org.springframework.amqp.rabbit.annotation.QueueBinding;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.messaging.handler.annotation.Payload;
+//import org.springframework.stereotype.Component;
+//
+//import static com.shelly.constants.MqConstant.*;
+//
+//
+///**
+// * 邮件消费者
+// *
+// * @author shelly
+// */
+//@Component
+//public class EmailConsumer {
+//
+//    @Autowired
+//    private EmailService emailService;
+//
+//    @RabbitListener(bindings = {
+//            @QueueBinding(
+//                    value = @Queue(value = EMAIL_SIMPLE_QUEUE, durable = "true", autoDelete = "false"),
+//                    exchange = @Exchange(value = EMAIL_EXCHANGE, type = ExchangeTypes.TOPIC),
+//                    key = EMAIL_SIMPLE_KEY
+//            )})
+//    public void listenSendSimpleEmail(@Payload MailDTO mail) {
+//        emailService.sendSimpleMail(mail);
+//    }
+//
+//    @RabbitListener(bindings = {
+//            @QueueBinding(
+//                    value = @Queue(value = EMAIL_HTML_QUEUE, durable = "true", autoDelete = "false"),
+//                    exchange = @Exchange(value = EMAIL_EXCHANGE, type = ExchangeTypes.TOPIC),
+//                    key = EMAIL_HTML_KEY
+//            )})
+//    public void listenSendHtmlEmail(@Payload MailDTO mail) {
+//        emailService.sendHtmlMail(mail);
+//    }
+//}
